6e3a86a2a4e042f3076b87e88129d28fbdbecc57
ChristianScarabelli <scachmabol@hotmail.it>
2024-12-06 12:56:28 +0100

    ADD: repo-set up: vite/react/react-dom-routing-bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center'>ciao</h1>
+    </>
+  )
+}
+
+export default App
