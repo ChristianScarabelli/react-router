24e9066ae95b6bc5dea3e6335823bee087de9307
ChristianScarabelli <scachmabol@hotmail.it>
2024-12-10 17:08:47 +0100

    ADD: loading post layout

--- src/pages/posts/Show.jsx
+++ src/pages/posts/Show.jsx
@@ -29,28 +29,43 @@ export default function Show() {
     return (
         <main>
             <div className="container my-3">
-                <div>
+                <div className="d-flex align-items-center justify-content-center gap-3">
                     <button className="btn btn-dark" onClick={() => navigate(-1)}>Prev</button>
-                    <button className="btn btn-dark" onClick={() => navigate(1)}>Next</button>
+                    <button className="btn btn-dark" onClick={() => navigate(+1)}>Next</button>
                 </div>
             </div>
             {post ?
                 <>
                     <div className="container">
                         <figure>
-                            <img className="img-fluid" src={post.image ? `${BASE_URI}/${post.image}` : placeHolderImage} alt={post.title} />
+                            <img className="img-fluid card-img-top" src={post.image ? `${BASE_URI}/${post.image}` : placeHolderImage} alt={post.title} />
                         </figure>
                         <h2>{post.title}</h2>
 
                         <h4 className="mt-2">{`Autore: ${post.author}`}</h4>
                         <h5 className=" mb-3 mt-2">{`Categoria: ${post.category}`}</h5>
                         <Tags tags={post.tags} />
-                        <p className="card-text mt-4">{post.content}</p>
+                        <p className="card-text mt-4 mb-4">{post.content}</p>
                     </div>
                 </> :
-                <div>
-                    loading
-                </div>
+                <>
+                    <div className="container py-3">
+                        <figure>
+                            <img className="img-fluid card-top-img" src={placeHolderImage} />
+                        </figure>
+                        <div className="placeholder-glow d-flex flex-column">
+                            <h2 className="placeholder bg-primary col-4"></h2>
+                            <h4 className="mt-2 placeholder bg-primary col-6"></h4>
+                            <h5 className=" mb-3 mt-2 placeholder bg-primary col-6"></h5>
+                            <Tags className='placeholder bg-primary col-10' />
+                            <p className=" mt-2 mb-1 placeholder bg-primary col-12"></p>
+                            <p className="mt-1 mb-1 placeholder bg-primary col-12"></p>
+                            <p className="mt-1 mb-1 placeholder bg-primary col-12"></p>
+                            <p className=" mt-1 mb-1 placeholder bg-primary col-12"></p>
+                        </div>
+
+                    </div>
+                </>
             }
         </main>
     )
